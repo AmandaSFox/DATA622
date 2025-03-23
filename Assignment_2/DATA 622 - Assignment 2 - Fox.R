e12d22e8827a6c09254f41b3b70fd64ee1dcaabd
Amanda Fox <Amanda.Fox02@spsmail.cuny.edu>
2025-03-23 14:03:19 -0400

    assignment 2

--- Assignment_2/DATA 622 - Assignment 2 - Fox.R
+++ Assignment_2/DATA 622 - Assignment 2 - Fox.R
@@ -2,54 +2,47 @@
 # DATA 622 Assignment 2
 # March 23, 2025
 
-# Experiment List: 
-# Decision tree (3)
-#   Feature engineering: 
-#       unbalanced with all features, 
-#       remove Duration (post hoc feature)
-#       balance Y, 
-# Random forest & adaboost (4)
-#   Default settings (2)
-#   Hyperparameter tuning - grid search (2) 
+# Experiment List:
+# Decision tree (2)
+#   unbalanced with all features 
+#   balanced Y 
+# Random forest (2)
+#   Default settings
+#   Hyperparameter tuning - grid search
+# Adaboost (2)
+#   Hyperparameter tuning - grid search (2)
 # Metamodel for ensemble method (1)
 
+# also tried with/without Duration
 
 #---------------------------------
 # Libraries
 #---------------------------------
 
 library(tidyverse)
+library(caret)
 library(ggplot2)
 library(rpart)
 library(rattle)
 library(ROSE)
 library(pROC)
-library(randomForest)
-library(themis)
-library(tidymodels)
-library(caret)
+library(doParallel)
 
-#library()
-#library()
-#library()
+#library(randomForest)
+#library(themis)
+#library(tidymodels)
 
 #---------------------------------
 # Preparations
 #---------------------------------
 
-#df <- read_csv("https://raw.githubusercontent.com/AmandaSFox/DATA622/refs/heads/main/Assignment_2/df_preprocessed.csv")
 
 # Load data
-df_all <- read_csv("df_preprocessed.csv")
-glimpse(df_all)
-
-# ELABORATE ON THIS IN ESSAY
-# since assignment #1, I changed to the dataset that included the external market features
-# some of these were highly correlated and the below code identifies and removes those features
-# (it also removes two features that I combined in assignment #1)
+df <- read_csv("df_preprocessed.csv")
+#df <- read_csv("https://raw.githubusercontent.com/AmandaSFox/DATA622/refs/heads/main/Assignment_2/df_preprocessed.csv")
 
 # find highly correlated features (newly added features)
-df_num <- df_all %>% 
+df_num <- df %>% 
   select(where(is.numeric))
 cor_matrix <- df_num %>% 
   cor(use = "pairwise.complete.obs")
@@ -60,15 +53,21 @@ cor_df <- as.data.frame(as.table(cor_matrix)) %>%
 
 cor_df
 
-df_dur <- df_all %>% 
-  select(-Housing_Loan, -Personal_Loan, -In_Default,
-         -emp.var.rate, -nr.employed)
-glimpse(df_dur)
+# Remove two of three correlated features
+df <- df %>% 
+  select(-emp.var.rate, -nr.employed)
+
+#Remove post hoc call duration feature
+df <- df %>% 
+  select(-Duration)
 
 # Change character cols to factors
-character_columns <- sapply(df_dur, is.character)
-df_dur[character_columns] <- lapply(df_dur[character_columns], as.factor)
-glimpse(df_dur)
+character_columns <- sapply(df, is.character)
+df[character_columns] <- lapply(df[character_columns], as.factor)
+
+glimpse(df)
+
+summary(df)
 
 # Initialize matrix to store metrics from each experiment
 matrix_metrics <- matrix(NA, nrow = 0, ncol = 6) 
@@ -79,111 +78,60 @@ colnames(matrix_metrics) <- c("Model",
                               "F1", 
                               "AUC")
 
+# Turn on parallel processing (chatgpt)
+
+num_cores <- detectCores() - 1 # Use one less core
+cl <- makePSOCKcluster(num_cores)
+registerDoParallel(cl)
+
 #---------------------------------
-# 1. Decision Tree: Unbalanced Y, includes call duration 
+# 1. Decision Tree: Unbalanced Y 
 #---------------------------------
 
 # Partition 70/30
 
-#set.seed(1)
-arr_sample_dur <- createDataPartition(y = df_dur$Y, 
+set.seed(1)
+arr_sample_unbal <- createDataPartition(y = df$Y, 
                                   p = .7, 
                                   list = FALSE)
-df_unbal_train_dur <- df_dur[arr_sample_dur, ]
-df_unbal_test_dur <- df_dur[-arr_sample_dur, ]
+df_unbal_train <- df[arr_sample_unbal, ]
+df_unbal_test <- df[-arr_sample_unbal, ]
 
 # Train first model
-model_dt_unbal_dur <- rpart(Y ~ ., 
+set.seed(123)
+model_dt_unbal <- rpart(Y ~ ., 
                     method = "class",
-                    data = df_unbal_train_dur)
+                    data = df_unbal_train)
 
-fancyRpartPlot(model_dt_unbal_dur)
+fancyRpartPlot(model_dt_unbal)
 
 # remove classes not found in train & refactor
 #df_unbal_test <- df_unbal_test[!(df_unbal_test$Occupation_Education %in% c("admin._illiterate", "housemaid_illiterate")), ]
 #df_unbal_test$Occupation_Education <- factor(df_unbal_test$Occupation_Education)
 
-
 # Test first model
-predictions_unbal_dur <- predict(model_dt_unbal_dur, 
-                              newdata = df_unbal_test_dur, 
+predictions_unbal <- predict(model_dt_unbal, 
+                              newdata = df_unbal_test, 
                               type = "class")
 
-cm_unbal_dur <- confusionMatrix(predictions_unbal_dur, 
-                            df_unbal_test_dur$Y)
+cm_unbal <- confusionMatrix(predictions_unbal, 
+                            df_unbal_test$Y)
 
-probabilities_unbal_dur <- predict(model_dt_unbal_dur, 
-                               newdata = df_unbal_test_dur, 
+probabilities_unbal <- predict(model_dt_unbal, 
+                               newdata = df_unbal_test, 
                                type = "prob")[, 2]
 
-auc_unbal_dur <- auc(roc(df_unbal_test_dur$Y, 
-                     probabilities_unbal_dur))
+auc_unbal <- auc(roc(df_unbal_test$Y, 
+                     probabilities_unbal))
 
 # Add metrics to the matrix
 matrix_metrics <- rbind(matrix_metrics, 
-                        c("1 Tree Unbalanced Y Duration",
-                          cm_unbal_dur$overall["Accuracy"],
-                          cm_unbal_dur$byClass["Precision"],
-                          cm_unbal_dur$byClass["Recall"],
-                          cm_unbal_dur$byClass["F1"],
-                          auc_unbal_dur))
-
-matrix_metrics
-
-#---------------------------------
-# 1b. Decision Tree: Unbalanced Y, excludes post hoc feature call duration
-#---------------------------------
-
-# EXPERIMENT: Remove duration feature
-df_no_dur <- df_dur %>% 
-  select(-Duration)
-glimpse(df_no_dur)
-
-# Partition 70/30
-
-#set.seed(12)
-arr_sample_no_dur <- createDataPartition(y = df_no_dur$Y, 
-                                  p = .7, 
-                                  list = FALSE)
-df_unbal_train_no_dur <- df_no_dur[arr_sample_no_dur, ]
-df_unbal_test_no_dur <- df_no_dur[-arr_sample_no_dur, ]
-
-# Train first model. 
-model_dt_unbal_no_dur <- rpart(
-                        Y ~ ., 
-                        data = df_unbal_train_no_dur,
-                        method = "class")
-
-fancyRpartPlot(model_dt_unbal_no_dur)
-
-# remove classes not found in train & refactor
-#df_unbal_test <- df_unbal_test[!(df_unbal_test$Occupation_Education %in% c("admin._illiterate", "housemaid_illiterate")), ]
-#df_unbal_test$Occupation_Education <- factor(df_unbal_test$Occupation_Education)
-
-
-# Test first model
-predictions_unbal_no_dur <- predict(model_dt_unbal_no_dur, 
-                             newdata = df_unbal_test_no_dur, 
-                             type = "class")
-
-cm_unbal_no_dur <- confusionMatrix(predictions_unbal_no_dur, 
-                            df_unbal_test_no_dur$Y)
-
-probabilities_unbal_no_dur <- predict(model_dt_unbal_no_dur, 
-                               newdata = df_unbal_test_no_dur, 
-                               type = "prob")[, 2]
-
-auc_unbal_no_dur <- auc(roc(df_unbal_test_no_dur$Y, 
-                     probabilities_unbal_no_dur))
-
-# Add metrics to the matrix
-matrix_metrics <- rbind(matrix_metrics, 
-                        c("1b Tree Unbalanced Y Removed Duration",
-                          cm_unbal_no_dur$overall["Accuracy"],
-                          cm_unbal_no_dur$byClass["Precision"],
-                          cm_unbal_no_dur$byClass["Recall"],
-                          cm_unbal_no_dur$byClass["F1"],
-                          auc_unbal_no_dur))
+                        c("1 Tree Unbalanced Y",
+                          cm_unbal$overall["Accuracy"],
+                          cm_unbal$byClass["Precision"],
+                          cm_unbal$byClass["Recall"],
+                          cm_unbal$byClass["F1"],
+                          auc_unbal))
 
 matrix_metrics
 
@@ -192,21 +140,20 @@ matrix_metrics
 #---------------------------------
 
 # show imbalance in Y
-df_no_dur %>%
+df %>%
   count(Y) %>%
   mutate(percentage = n / sum(n) * 100)
 
 # Balance using ROSE()
-df_bal <- ROSE(Y ~ ., data = df_no_dur)$data
+df_bal <- ROSE(Y ~ ., data = df)$data
 
 # Check the new balance
 df_bal %>%
   count(Y) %>%
   mutate(percentage = n / sum(n) * 100)
 
-# Make a new tree with balanced data
-
-#set.seed(2)
+# Partition balanced data <- use for all remaining models
+set.seed(2)
 arr_sample_bal <- createDataPartition(y = df_bal$Y, 
                                       p = .7, 
                                       list = FALSE)
@@ -214,6 +161,7 @@ df_bal_train <- df_bal[arr_sample_bal, ]
 df_bal_test <- df_bal[-arr_sample_bal, ]
 
 # Train model
+set.seed(234)
 dt_bal <- rpart(Y ~ ., 
                   method = "class",
                   data = df_bal_train)
@@ -221,28 +169,28 @@ dt_bal <- rpart(Y ~ .,
 fancyRpartPlot(dt_bal)
 
 # Test model
-predictions_bal <- predict(dt_bal, 
+predictions_dt_bal <- predict(dt_bal, 
                              newdata = df_bal_test, 
                              type = "class")
 
-cm_bal <- confusionMatrix(predictions_bal, 
+cm_dt_bal <- confusionMatrix(predictions_dt_bal, 
                             df_bal_test$Y)
 
-probabilities_bal <- predict(dt_bal, 
+probabilities_dt_bal <- predict(dt_bal, 
                                newdata = df_bal_test, 
                                type = "prob")[, 2]
 
-auc_bal <- auc(roc(df_bal_test$Y, 
-                     probabilities_bal))
+auc_dt_bal <- auc(roc(df_bal_test$Y, 
+                     probabilities_dt_bal))
 
 # Add metrics to the matrix
 matrix_metrics <- rbind(matrix_metrics, 
                         c("2 Tree Balanced Y",
-                          cm_bal$overall["Accuracy"],
-                          cm_bal$byClass["Precision"],
-                          cm_bal$byClass["Recall"],
-                          cm_bal$byClass["F1"],
-                          auc_bal))
+                          cm_dt_bal$overall["Accuracy"],
+                          cm_dt_bal$byClass["Precision"],
+                          cm_dt_bal$byClass["Recall"],
+                          cm_dt_bal$byClass["F1"],
+                          auc_dt_bal))
 
 matrix_metrics
 
@@ -252,37 +200,40 @@ matrix_metrics
 
 # Using same train/test dataset as above (balanced Y)
 
-# Train model
-rf_default <- randomForest(Y ~ ., 
-                           data = df_bal_train)
+# Train model (random forest package default: Random Forest model trained using the randomForest package with default settings. 
+#This model was trained on the entire training dataset without any resampling, 
+#cross-validation, or bootstrapping, resulting in a single Random Forest model)
 
+set.seed(345)
+rf_default <- randomForest::randomForest(Y~.,
+                                         data = df_bal_train)
 
 rf_default_predictions <- predict(rf_default, 
                                   df_bal_test)
 
 # Test model
-predictions_bal <- predict(dt_bal, 
+predictions_rf_bal <- predict(rf_default, 
                              newdata = df_bal_test, 
                              type = "class")
 
-cm_bal <- confusionMatrix(predictions_bal, 
+cm_rf_bal <- confusionMatrix(predictions_rf_bal, 
                             df_bal_test$Y)
 
-probabilities_bal <- predict(dt_bal, 
+probabilities_rf_bal <- predict(rf_default, 
                                newdata = df_bal_test, 
                                type = "prob")[, 2]
 
-auc_bal <- auc(roc(df_bal_test$Y, 
-                     probabilities_bal))
+auc_rf_bal <- auc(roc(df_bal_test$Y, 
+                     probabilities_rf_bal))
 
 # Add metrics to the matrix
 matrix_metrics <- rbind(matrix_metrics, 
-                        c("2 Tree Balanced Y",
-                          cm_bal$overall["Accuracy"],
-                          cm_bal$byClass["Precision"],
-                          cm_bal$byClass["Recall"],
-                          cm_bal$byClass["F1"],
-                          auc_bal))
+                        c("3 Random Forest Default",
+                          cm_rf_bal$overall["Accuracy"],
+                          cm_rf_bal$byClass["Precision"],
+                          cm_rf_bal$byClass["Recall"],
+                          cm_rf_bal$byClass["F1"],
+                          auc_rf_bal))
 
 matrix_metrics
 
@@ -291,21 +242,167 @@ matrix_metrics
 # 4. Random Forest: Hyperparameter tuned (grid)
 #---------------------------------
 
-# Tuned Random Forest
-# tuneGrid <- expand.grid(.mtry=c(2,3,4))
-# rf_tuned <- train(Y ~ ., data = train_df, method = "rf", tuneGrid = tuneGrid)
-# rf_tuned_predictions <- predict(rf_tuned, test_df)
+# Define tuning grid for mtry values, five-fold cross-validation
+rf_tuneGrid <- expand.grid(mtry=c(2,4,6,8,10,12)) #was 2,4,6,8
+rf_control <- trainControl(method = "cv", number = 5)
 
-# Evaluate your models using rf_default_predictions and rf_tuned_predictions
+
+# Train model (caret package with hyperparameter tuning)
+set.seed(456)
+rf_tuned <- caret::train(Y ~ ., 
+                  data = df_bal_train, 
+                  method = "rf", 
+                  trControl = rf_control, 
+                  tuneGrid = rf_tuneGrid)
+
+# View caret's tuning selection results 
+rf_tuned$results
+rf_tuned$bestTune
+rf_tuned$finalModel
+
+# Test model
+predictions_rf_tuned <- predict(rf_tuned, 
+                              newdata = df_bal_test, 
+                              type = "raw") #caret requires raw or prob
+
+cm_rf_tuned <- confusionMatrix(predictions_rf_tuned, 
+                             df_bal_test$Y)
+
+probabilities_rf_tuned <- predict(rf_tuned, 
+                                newdata = df_bal_test, 
+                                type = "prob")[, 2]
+
+auc_rf_tuned <- auc(roc(df_bal_test$Y, 
+                      probabilities_rf_tuned))
+
+# Add metrics to the matrix
+matrix_metrics <- rbind(matrix_metrics, 
+                        c("4 Random Forest Tuned",
+                          cm_rf_tuned$overall["Accuracy"],
+                          cm_rf_tuned$byClass["Precision"],
+                          cm_rf_tuned$byClass["Recall"],
+                          cm_rf_tuned$byClass["F1"],
+                          auc_rf_tuned))
+
+matrix_metrics
+
+#---------------------------------
+# 5. Adaboost: Default model errored out:  There were missing values in resampled performance measures.
+#---------------------------------
+
+# Additional data prep for adaboost (chatgpt)
+ada_train <- droplevels(df_bal_train)
+summary(ada_train)
+
+# Train default AdaBoost model
+set.seed(567)
+ada_default <- train(Y ~ ., 
+                     data = ada_train, 
+                     method = "ada")
+
+
+# Test model
+predictions_ada <- predict(ada_default, 
+                              newdata = df_bal_test, 
+                              type = "class")
+
+cm_ada <- confusionMatrix(predictions_ada, 
+                             df_bal_test$Y)
+
+probabilities_ada <- predict(ada_default, 
+                                newdata = df_bal_test, 
+                                type = "prob")[, 2]
+
+auc_ada <- auc(roc(df_bal_test$Y, 
+                      probabilities_ada))
+
+# Add metrics to the matrix
+matrix_metrics <- rbind(matrix_metrics, 
+                        c("5 Adaboost Default",
+                          cm_ada$overall["Accuracy"],
+                          cm_ada$byClass["Precision"],
+                          cm_ada$byClass["Recall"],
+                          cm_ada$byClass["F1"],
+                          auc_ada))
+
+matrix_metrics
 
 
 #---------------------------------
-# 5. Adaboost: Balanced (SMOTE), hyperparameter tuned (grid)
+# 5b. Adaboost: Hyperparameter tuned (grid)
 #---------------------------------
 
+# Additional data prep for adaboost (chatgpt)
+ada_train <- droplevels(df_bal_train)
+str(ada_train)
+
+# Find tunable parameters
+# getModelInfo("ada") 
+
+# Define tuning grid for mtry values, five-fold cross-validation
+# add classProbs = TRUE and twoClassSummary
+
+ada_tuneGrid <- expand.grid(iter = c(25,50,75),
+                            maxdepth = c(1,2),
+                            nu = c(0.01,0.1))
+
+ada_control <- trainControl(method = "cv", 
+                            number = 5,
+                            classProbs = TRUE,
+                            summaryFunction = twoClassSummary)
+
+install.packages("devtools")  # if not already installed
+devtools::install_github("kassambara/fastAdaboost")
+library(fastAdaboost)
+
+# Train model (fastAdaboost package with hyperparameter tuning)
+#set.seed(678)
+ada_tuned <- caret::train(Y ~ ., 
+                         data = ada_train, 
+                         method = "ada", 
+                         trControl = ada_control, 
+                         tuneGrid = ada_tuneGrid,
+                         preProcess = c("center","scale"))
+
+# View caret's tuning selection results 
+ada_tuned$bestTune
+ada_tuned$results
+ada_tuned$finalModel
+
+# Test model
+predictions_ada_tuned <- predict(ada_tuned, 
+                                newdata = df_bal_test, 
+                                type = "class")
+
+cm_ada_tuned <- confusionMatrix(predictions_ada_tuned, 
+                               df_bal_test$Y)
+
+probabilities_ada_tuned <- predict(ada_tuned, 
+                                  newdata = df_bal_test, 
+                                  type = "prob")[, 2]
+
+auc_ada_tuned <- auc(roc(df_bal_test$Y, 
+                        probabilities_ada_tuned))
+
+# Add metrics to the matrix
+matrix_metrics <- rbind(matrix_metrics, 
+                        c("6 Adaboost Tuned",
+                          cm_ada_tuned$overall["Accuracy"],
+                          cm_ada_tuned$byClass["Precision"],
+                          cm_ada_tuned$byClass["Recall"],
+                          cm_ada_tuned$byClass["F1"],
+                          auc_ada_tuned))
+
+matrix_metrics
 
 
 #---------------------------------
 # 6. Metamodel Ensemble: DT balanced, RF balanced/tuned, 
 # Adaboost balanced/tuned
 #---------------------------------
+
+
+# parallel processing stop (chatgpt)
+stopCluster(cl)
+registerDoSEQ()
+
